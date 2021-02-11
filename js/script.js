393f71911fd0ef4e3d0c085952520357252e6430
Jennifer LaRue <jennifer@photoshelter.com>
2021-02-11 09:05:05 -0500

    step 4 completed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,26 @@
 const nameInput = document.querySelector('input[type="text"]');
 nameInput.focus();
 
-//Selecting Job Role Element
+//Selecting Job Role Select Element
+const jobRole = document.getElementById('title');
+console.log(jobRole);
+
+//Selecting Other Job Role Element & setting its display style to be hidden
+const otherJobRole = document.getElementById('other-job-role');
+console.log(otherJobRole);
+otherJobRole.style.display = 'none';
+
+//Setting an event listener to watch job role element
+jobRole.addEventListener('change', (e) => {
+    const jobSelected = e.target.value;
+    if(jobSelected === 'other') {
+        otherJobRole.style.display = 'block';
+    } else {
+        otherJobRole.style.display = 'none';
+    }
+});
+
+
 
 
 
