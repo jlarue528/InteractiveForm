1807bf0fd05a07ef0f7f4602bb426d3492050186
Jennifer LaRue <jennifer@photoshelter.com>
2021-02-11 10:36:54 -0500

    step 5 complete

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ nameInput.focus();
 
 //Selecting Job Role Select Element
 const jobRole = document.getElementById('title');
-console.log(jobRole);
+// console.log(jobRole);
 
 //Selecting Other Job Role Element & setting its display style to be hidden
 const otherJobRole = document.getElementById('other-job-role');
-console.log(otherJobRole);
+// console.log(otherJobRole);
 otherJobRole.style.display = 'none';
 
 //Setting an event listener to watch job role element
@@ -21,6 +21,36 @@ jobRole.addEventListener('change', (e) => {
     }
 });
 
+//Selecting design & color elements
+const design = document.getElementById('design');
+const color = document.getElementById('color');
+const colorOptions = color.children;
+// console.log(colorOptions);
+
+//Setting color property to disabled
+color.disabled = true;
+
+//Add event listener to tshirt design dropdown 
+design.addEventListener('change', (e) => {
+   color.disabled = false;
+   
+   for(let i = 1; i < colorOptions.length; i++) {
+       //dropdown theme selection
+       let themeSelection = e.target.value;
+       //data theme attribute of selected theme
+        let dataTheme = colorOptions[i].getAttribute('data-theme');
+
+        //conditional statement - based on user's theme selection
+        //the color options for t-shirts will display
+        if (themeSelection === dataTheme) {
+            colorOptions[i].hidden = false;
+            colorOptions[i].setAttribute('selected', true);
+        } else {
+            colorOptions[i].hidden = true;
+            colorOptions[i].setAttribute('selected', false);
+        }
+   }
+});
 
 
 
